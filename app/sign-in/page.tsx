5e8e296cfcddce77c36348ea052571eb9c7b9f68
Hanu Dixit <Hanudixit.2017@gmail.com>
2026-05-13 17:38:20 -0400

    Add Clerk sign in and sign up pages

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-50 px-4">
+      <SignIn />
+    </main>
+  );
+}
